6d435fab249a40d15553c5ad4045053c819b7c21
Wojciech Malarski <w.malarski@gmail.com>
2022-09-10 17:23:41 +0200

    improving links

--- src/modules/Sidebar/Sidebar.tsx
+++ src/modules/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { JSX } from "solid-js";
+import { NavLink } from "solid-start";
 import { paths } from "~/utils/paths";
 import { supabase } from "~/utils/supabase";
 
@@ -12,10 +13,14 @@ export const Sidebar = (): JSX.Element => {
   return (
     <ul class="menu bg-base-100 w-56 p-2 rounded-box">
       <li>
-        <a href={paths.index}>Home</a>
+        <NavLink activeClass="active" href={paths.index}>
+          Home
+        </NavLink>
       </li>
       <li>
-        <a href={paths.about}>About</a>
+        <NavLink activeClass="active" href={paths.about}>
+          About
+        </NavLink>
       </li>
       <li>
         <button onClick={handleLogout}>Logout</button>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,6 @@
-import { Link } from "@solidjs/router";
 import { JSX } from "solid-js";
-import Counter from "~/components/Counter";
+import { Outlet } from "solid-start";
 import { Sidebar } from "~/modules/Sidebar/Sidebar";
-import { paths } from "~/utils/paths";
 import { supabase } from "~/utils/supabase";
 
 // export function routeData() {
@@ -19,31 +17,8 @@ const Home = (): JSX.Element => {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <Sidebar />
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <Counter />
       <pre>{JSON.stringify(session, null, 2)}</pre>
-      <p class="mt-8">
-        Visit{" "}
-        <Link
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </Link>
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        <Link href={paths.about} class="text-sky-600 hover:underline">
-          About Page
-        </Link>
-        <Link href={paths.login} class="text-sky-600 hover:underline">
-          Login
-        </Link>
-      </p>
+      <Outlet />
     </main>
   );
 };
