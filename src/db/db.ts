65f04036bbed82642d3c780267801462c491a701
Wojciech Malarski <w.malarski@gmail.com>
2023-04-08 16:38:49 +0200

    drizzle installed

---
+++ src/db/db.ts
@@ -0,0 +1,23 @@
+import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var db: NodePgDatabase | undefined;
+}
+
+export const createDrizzle = () => {
+  const pool = new Pool({
+    connectionString: env.DATABASE_URL,
+  });
+
+  return drizzle(pool);
+};
+
+export const db =
+  typeof global !== "undefined"
+    ? global.db || createDrizzle()
+    : createDrizzle();
+
+if (process.env.NODE_ENV !== "production" && typeof global !== "undefined") {
+  global.db = db;
+}
