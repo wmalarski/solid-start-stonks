9816a1da2a7bd43178313c2d32a909e8dedb9696
Wojciech Malarski <w.malarski@gmail.com>
2023-08-26 11:17:01 +0200

    improved invoice parsing

--- src/server/invoices/api.ts
+++ src/server/invoices/api.ts
@@ -1,77 +1,49 @@
-import type { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
 import type { FetchEvent } from "solid-start";
-import { queryNotionDatabase } from "../notion";
+import {
+  databaseResponseToProperties,
+  dateToPlainDate,
+  numberToPlainNumber,
+  queryNotionDatabase,
+  richTextToPlainText,
+  titleToPlainText,
+  uniqueIdToPlainText,
+  type QueryDatabaseResult,
+} from "../notion";
 
-const databaseResponseToInvoice = (
-  response: QueryDatabaseResponse["results"][0],
-) => {
-  if ("properties" in response) {
-    const properties = response.properties;
+const databaseResponseToInvoice = (result: QueryDatabaseResult) => {
+  const properties = databaseResponseToProperties(result);
 
-    const titleToPlainText = (property: (typeof properties)[0]) => {
-      if (property.type !== "title") {
-        throw new Error(`parsing-error: ${property.type}`);
-      }
-      return property.title.map((entry) => entry.plain_text).join(" ");
-    };
-
-    const richTextToPlainText = (property: (typeof properties)[0]) => {
-      if (property.type !== "rich_text") {
-        throw new Error(`parsing-error: ${property.type}`);
-      }
-      return property.rich_text.map((entry) => entry.plain_text).join(" ");
-    };
-
-    const numberToPlainNumber = (property: (typeof properties)[0]) => {
-      if (property.type !== "number" || typeof property.number !== "number") {
-        throw new Error(`parsing-error: ${property.type}`);
-      }
-      return property.number;
-    };
-
-    const dateToPlainDate = (property: (typeof properties)[0]) => {
-      if (property.type !== "date" || !property.date?.start) {
-        throw new Error(`parsing-error: ${property.type}`);
-      }
-      return property.date.start;
-    };
-
-    const uniqueIdToPlainText = (property: (typeof properties)[0]) => {
-      if (property.type !== "unique_id" || !property.unique_id.number) {
-        throw new Error(`parsing-error: ${property.type}`);
-      }
-      return property.unique_id.number;
-    };
-
-    try {
-      return {
-        buyerAddress1: titleToPlainText(properties.buyerAddress1),
-        buyerAddress2: richTextToPlainText(properties.buyerAddress2),
-        buyerName: richTextToPlainText(properties.buyerName),
-        buyerNip: richTextToPlainText(properties.buyerNip),
-        city: richTextToPlainText(properties.city),
-        date: dateToPlainDate(properties.date),
-        id: uniqueIdToPlainText(properties.ID),
-        invoiceTitle: richTextToPlainText(properties.invoiceTitle),
-        notes: richTextToPlainText(properties.notes),
-        paymentAccount: richTextToPlainText(properties.paymentAccount),
-        paymentBank: richTextToPlainText(properties.paymentBank),
-        paymentMethod: richTextToPlainText(properties.paymentMethod),
-        sellerAddress1: richTextToPlainText(properties.sellerAddress1),
-        sellerAddress2: richTextToPlainText(properties.sellerAddress2),
-        sellerName: richTextToPlainText(properties.sellerName),
-        sellerNip: richTextToPlainText(properties.sellerNip),
-        serviceCount: numberToPlainNumber(properties.serviceCount),
-        servicePayed: numberToPlainNumber(properties.servicePayed),
-        servicePrice: numberToPlainNumber(properties.servicePrice),
-        serviceTitle: richTextToPlainText(properties.serviceTitle),
-        serviceUnit: richTextToPlainText(properties.serviceUnit),
-      };
-    } catch (err) {
-      return null;
-    }
+  if (!properties) {
+    return null;
   }
-  return null;
+
+  try {
+    return {
+      buyerAddress1: titleToPlainText(properties.buyerAddress1),
+      buyerAddress2: richTextToPlainText(properties.buyerAddress2),
+      buyerName: richTextToPlainText(properties.buyerName),
+      buyerNip: richTextToPlainText(properties.buyerNip),
+      city: richTextToPlainText(properties.city),
+      date: dateToPlainDate(properties.date),
+      id: uniqueIdToPlainText(properties.ID),
+      invoiceTitle: richTextToPlainText(properties.invoiceTitle),
+      notes: richTextToPlainText(properties.notes),
+      paymentAccount: richTextToPlainText(properties.paymentAccount),
+      paymentBank: richTextToPlainText(properties.paymentBank),
+      paymentMethod: richTextToPlainText(properties.paymentMethod),
+      sellerAddress1: richTextToPlainText(properties.sellerAddress1),
+      sellerAddress2: richTextToPlainText(properties.sellerAddress2),
+      sellerName: richTextToPlainText(properties.sellerName),
+      sellerNip: richTextToPlainText(properties.sellerNip),
+      serviceCount: numberToPlainNumber(properties.serviceCount),
+      servicePayed: numberToPlainNumber(properties.servicePayed),
+      servicePrice: numberToPlainNumber(properties.servicePrice),
+      serviceTitle: richTextToPlainText(properties.serviceTitle),
+      serviceUnit: richTextToPlainText(properties.serviceUnit),
+    };
+  } catch (err) {
+    return null;
+  }
 };
 
 export type Invoice = NonNullable<ReturnType<typeof databaseResponseToInvoice>>;
@@ -113,6 +85,30 @@ type QueryNotionInvoiceArgs = {
 };
 
 export const queryNotionInvoice = async ({
+  event,
+  id,
+}: QueryNotionInvoiceArgs) => {
+  const response = await queryNotionDatabase({
+    event,
+    filter: { number: { equals: id }, property: "ID" },
+    page_size: 1,
+  });
+
+  const result = response.results.at(0);
+
+  if (!result) {
+    return null;
+  }
+
+  return databaseResponseToInvoice(result);
+};
+
+type InsertNotionInvoiceArgs = {
+  event: FetchEvent;
+  id: number;
+};
+
+export const insertNotionInvoice = async ({
   event,
   id,
 }: QueryNotionInvoiceArgs) => {
--- src/server/notion.ts
+++ src/server/notion.ts
@@ -1,5 +1,8 @@
 import { Client } from "@notionhq/client";
-import type { QueryDatabaseParameters } from "@notionhq/client/build/src/api-endpoints";
+import type {
+  QueryDatabaseParameters,
+  QueryDatabaseResponse,
+} from "@notionhq/client/build/src/api-endpoints";
 import type { FetchEvent } from "solid-start";
 import { serverEnv } from "./env";
 
@@ -21,6 +24,55 @@ const getNotionClient = (event: FetchEvent): Client => {
   return notion;
 };
 
+export type QueryDatabaseResult = QueryDatabaseResponse["results"][0];
+
+export const databaseResponseToProperties = (
+  response: QueryDatabaseResponse["results"][0],
+) => {
+  return "properties" in response ? response.properties : null;
+};
+
+type QueryDatabaseProperties = NonNullable<
+  ReturnType<typeof databaseResponseToProperties>
+>;
+
+type QueryDatabaseProperty = QueryDatabaseProperties[0];
+
+export const titleToPlainText = (property: QueryDatabaseProperty) => {
+  if (property.type !== "title") {
+    throw new Error(`parsing-error: ${property.type}`);
+  }
+  return property.title.map((entry) => entry.plain_text).join(" ");
+};
+
+export const richTextToPlainText = (property: QueryDatabaseProperty) => {
+  if (property.type !== "rich_text") {
+    throw new Error(`parsing-error: ${property.type}`);
+  }
+  return property.rich_text.map((entry) => entry.plain_text).join(" ");
+};
+
+export const numberToPlainNumber = (property: QueryDatabaseProperty) => {
+  if (property.type !== "number" || typeof property.number !== "number") {
+    throw new Error(`parsing-error: ${property.type}`);
+  }
+  return property.number;
+};
+
+export const dateToPlainDate = (property: QueryDatabaseProperty) => {
+  if (property.type !== "date" || !property.date?.start) {
+    throw new Error(`parsing-error: ${property.type}`);
+  }
+  return property.date.start;
+};
+
+export const uniqueIdToPlainText = (property: QueryDatabaseProperty) => {
+  if (property.type !== "unique_id" || !property.unique_id.number) {
+    throw new Error(`parsing-error: ${property.type}`);
+  }
+  return property.unique_id.number;
+};
+
 export const getNotionUsers = (event: FetchEvent) => {
   const notion = getNotionClient(event);
 
@@ -39,4 +91,22 @@ export const queryNotionDatabase = ({
   const notion = getNotionClient(event);
 
   return notion.databases.query({ database_id: env.notionDatabase, ...args });
+};
+
+type InsertNotionDatabaseArgs = Omit<QueryDatabaseParameters, "database_id"> & {
+  event: FetchEvent;
+};
+
+export const insertNotionDatabase = ({
+  event,
+  ...args
+}: InsertNotionDatabaseArgs) => {
+  const env = serverEnv(event);
+  const notion = getNotionClient(event);
+
+  return notion.databases.create({
+    parent: { database_id: env.notionDatabase, type: "database_id" },
+    properties: {},
+    // ...args,
+  });
 };
